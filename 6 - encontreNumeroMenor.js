1bda94d1c0aef183de5c970c78b4955abd4381f6
Flavio Silva <flavioms86@hotmail.com>
2023-08-06 19:23:33 -0300

    Resolução do desafio

--- 6 - encontreNumeroMenor.js
+++ 6 - encontreNumeroMenor.js
@@ -1,2 +1,23 @@
+// Desafio 6:
+// Escreva uma função chamada encontreNumeroMenor que 
+// recebe um array de números como parâmetro e retorna 
+// o menor número presente nesse array.
+
+// Exemplo de entrada e saída:
+
 const numeros = [10, 4, 7, 15, 2];
 console.log(encontreNumeroMenor(numeros)); // Deve imprimir 2
+
+// Resolução
+
+function encontreNumeroMenor(numeros) {
+    let menorNumero = numeros[0];
+
+    for (let numero of numeros) {
+        if (numero < menorNumero) {
+            menorNumero = numero;
+        }
+    }
+
+    return menorNumero;
+}
