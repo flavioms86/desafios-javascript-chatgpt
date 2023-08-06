32725eb2d474e2377fecb83a1a504940c852a3ae
Flavio Silva <flavioms86@hotmail.com>
2023-08-06 19:27:23 -0300

    Resolução do desafio

--- 8 - conteVogais.js
+++ 8 - conteVogais.js
@@ -1,2 +1,24 @@
+// Desafio 8:
+// Escreva uma função chamada conteVogais que recebe uma 
+// string como parâmetro e retorna a quantidade de vogais 
+// (letras "a", "e", "i", "o" e "u") presentes na string. 
+// A contagem deve ser case-insensitive, ou seja, deve 
+// considerar tanto vogais maiúsculas como minúsculas.
+
+// Exemplo de entrada e saída:
+
 const texto = "Olá, estou praticando JavaScript!";
 console.log(conteVogais(texto)); // Deve imprimir 10
+
+// Resolução
+
+function conteVogais(texto) {
+    let contador = 0;
+    for (let caractere of texto) {
+        if (caractere === "a" || caractere === "A" || caractere === "e" || caractere === "E" || caractere === "i" || caractere === "I" || caractere === "o" || caractere === "O" || caractere === "u" || caractere === "U") {
+            contador++;
+        }
+    }
+
+    return contador;
+}
