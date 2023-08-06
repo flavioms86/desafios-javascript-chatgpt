2d5a838ccfd4432d7b7321a3fc03f844419469cd
Flavio Silva <flavioms86@hotmail.com>
2023-08-06 20:10:38 -0300

    Definição do desafio

---
+++ 23 - conteCaracteresRepetidos.js
@@ -0,0 +1,20 @@
+// Desafio 23:
+// Escreva uma função chamada conteCaracteresPalavras 
+// que recebe uma string como parâmetro e retorna um objeto 
+// contendo a quantidade de caracteres e a quantidade de 
+// palavras na string. O objeto deve ter o seguinte formato:
+
+/*
+{
+    "caracteres": quantidadeCaracteres,
+    "palavras": quantidadePalavras
+  }
+*/
+
+// Considere que as palavras são separadas por espaços em branco.
+
+// Exemplo de entrada e saída:
+
+const texto = "Eu gosto de programar em JavaScript.";
+console.log(conteCaracteresPalavras(texto)); // Deve imprimir { "caracteres": 36, "palavras": 6 }
+
