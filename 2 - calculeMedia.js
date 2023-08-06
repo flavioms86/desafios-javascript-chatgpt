9ff89031d67fffc9e7c86331db396915353dfb24
Flavio Silva <flavioms86@hotmail.com>
2023-08-06 19:09:59 -0300

    Resolução do desafio

--- 2 - calculeMedia.js
+++ 2 - calculeMedia.js
@@ -1,2 +1,20 @@
+// Desafio 2:
+// Escreva uma função chamada calculeMedia que recebe 
+// um array de números como parâmetro e retorna a média 
+// aritmética desses números.
+
+// Exemplo de entrada e saída:
+
 const numeros = [10, 20, 30, 40, 50];
 console.log(calculeMedia(numeros)); // Deve imprimir 30
+
+// Resolução
+
+function calculeMedia(numeros) {
+    let somaNumeros = 0;
+    for (let numero of numeros) {
+        somaNumeros += numero;
+    }
+
+    return somaNumeros / numeros.length;
+}
