c45d41cadf362059391fbfc91d74fe806af51210
Flavio Silva <flavioms86@hotmail.com>
2023-08-06 19:06:10 -0300

    Resolução do desafio

--- 1 - encontreMaiorNumero.js
+++ 1 - encontreMaiorNumero.js
@@ -1,6 +1,15 @@
+// Desafio 1:
+// Escreva uma função chamada encontreMaiorNumero que 
+// recebe um array de números como parâmetro e retorna o 
+// maior número presente nesse array.
+
+// Exemplo de entrada e saída:
+
 const numeros = [10, 4, 7, 15, 2];
 
+// Resolução
 function encontreMaiorNumero(numeros) {
+
     let maiorNumero = 0;
     for (let numero of numeros) {
         if (numero > maiorNumero) {
