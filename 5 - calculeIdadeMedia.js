84fbe9725d7c0dbd1d3b9e8e1448e11f6294990f
Flavio Silva <flavioms86@hotmail.com>
2023-08-06 19:20:54 -0300

    Resolução do desafio

--- 5 - calculeIdadeMedia.js
+++ 5 - calculeIdadeMedia.js
@@ -1,2 +1,19 @@
+// Desafio 5:
+// Escreva uma função chamada calculeIdadeMedia que 
+// recebe um array de idades como parâmetro e retorna a 
+// média das idades.
+
+// Exemplo de entrada e saída:
+
 const idades = [18, 20, 25, 30];
 console.log(calculeIdadeMedia(idades)); // Deve imprimir 23.25
+
+// Resolução
+
+function calculeIdadeMedia(idades) {
+    let somaIdade = 0;
+    for (let idade of idades) {
+        somaIdade += idade;
+    }
+    return somaIdade / idades.length
+}
