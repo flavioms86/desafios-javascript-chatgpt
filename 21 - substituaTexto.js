6667eee4fcbe0295cc98822f4c3abb64a79ee741
Flavio Silva <flavioms86@hotmail.com>
2023-08-06 19:01:53 -0300

    Problema inicial

---
+++ 21 - substituaTexto.js
@@ -0,0 +1,2 @@
+const frase = "O JavaScript é uma linguagem de programação. A linguagem JavaScript é muito popular.";
+console.log(substituaTexto(frase, "JavaScript", "Python")); // Deve imprimir "O Python é uma linguagem de programação. A linguagem Python é muito popular."
