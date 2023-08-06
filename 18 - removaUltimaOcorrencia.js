24eaf7164f7c68b637a60504dc98c53732754988
Flavio Silva <flavioms86@hotmail.com>
2023-08-06 18:59:49 -0300

    Problema inicial

---
+++ 18 - removaUltimaOcorrencia.js
@@ -0,0 +1,2 @@
+const frase = "O JavaScript é uma linguagem de programação. A linguagem JavaScript é muito popular.";
+console.log(removaUltimaOcorrencia(frase, "JavaScript")); // Deve imprimir "O JavaScript é uma linguagem de programação. A linguagem  é muito popular."
