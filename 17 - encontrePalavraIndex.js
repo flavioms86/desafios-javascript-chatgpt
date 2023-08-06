b3f5b0d6011dc51d046fbf66e70ba6f3645e893c
Flavio Silva <flavioms86@hotmail.com>
2023-08-06 19:55:37 -0300

    Resolução do desafio

--- 17 - encontrePalavraIndex.js
+++ 17 - encontrePalavraIndex.js
@@ -1,2 +1,16 @@
+// Desafio 17:
+// Escreva uma função chamada encontrePalavraIndex que 
+// recebe duas strings como parâmetros: uma frase e uma palavra. 
+// A função deve retornar o índice da primeira ocorrência da 
+// palavra na frase utilizando o método .indexOf().
+
+// Exemplo de entrada e saída:
+
 const frase = "Eu gosto de programar em JavaScript. JavaScript é incrível!";
 console.log(encontrePalavraIndex(frase, "JavaScript")); // Deve imprimir 19
+
+// Resolução
+
+function encontrePalavraIndex(frase, palavra) {
+    return frase.indexOf(palavra);
+}
