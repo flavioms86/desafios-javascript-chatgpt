b22343429921bf8159ac98d0d5d78639bc23515b
Flavio Silva <flavioms86@hotmail.com>
2023-08-06 19:54:06 -0300

    Resolução do desafio

--- 16 - conteSubstring.js
+++ 16 - conteSubstring.js
@@ -1,2 +1,28 @@
+// Desafio 16:
+// Escreva uma função chamada conteSubstring que 
+// recebe duas strings como parâmetros e retorna o número de 
+// ocorrências da segunda string na primeira. Utilize 
+// o método .includes() para verificar as ocorrências.
+
+// Exemplo de entrada e saída:
+
 const texto = "Olá, tudo bem? Olá, sim, tudo bem!";
 console.log(conteSubstring(texto, "Olá")); // Deve imprimir 2
+
+// Resolução
+
+function conteSubstring(texto, string) {
+    let contador = 0;
+    const palavras = separarPalavras(texto);
+    for (let palavra of palavras) {
+        if (palavra.includes(string)) {
+            contador++
+        };
+    }
+    return contador;
+}
+
+function separarPalavras(texto) {
+    const palavras = texto.split(" ");
+    return palavras;
+}
