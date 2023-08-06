d65e1ce7d089f5aaee3f2c0b4649758de62cf0ad
Flavio Silva <flavioms86@hotmail.com>
2023-08-06 19:59:13 -0300

    Resolução do desafio

--- 19 - converteMaiusculaPrimeiraLetra.js
+++ 19 - converteMaiusculaPrimeiraLetra.js
@@ -1,2 +1,18 @@
+// Desafio 19:
+// Escreva uma função chamada converteMaiusculaPrimeiraLetra 
+// que recebe uma string como parâmetro e retorna a mesma string 
+// com a primeira letra em maiúscula utilizando o método .toUpperCase() 
+// e .slice().
+
+// Exemplo de entrada e saída:
+
 const texto = "javascript é uma linguagem de programação.";
 console.log(converteMaiusculaPrimeiraLetra(texto)); // Deve imprimir "Javascript é uma linguagem de programação."
+
+// Resolução
+
+function converteMaiusculaPrimeiraLetra(texto) {
+    let primeiraLetra = texto.slice(0, 1);
+    let restoTexto = texto.slice(1);
+    return `${primeiraLetra.toUpperCase()}${restoTexto}`;
+}
